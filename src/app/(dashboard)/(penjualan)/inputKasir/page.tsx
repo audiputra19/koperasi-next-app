f477f28e09fa6b893e6d18df2cbf8ab681186dec
audiputra19 <audiputra19@gmail.com>
2026-08-03 08:45:44 +0700

    update loading for input kasir and try some log

--- src/app/(dashboard)/(penjualan)/inputKasir/page.tsx
+++ src/app/(dashboard)/(penjualan)/inputKasir/page.tsx
@@ -4,10 +4,14 @@ import { MenuService } from "@/src/features/menu/menu.service";
 import { getSession } from "@/src/lib/session";
 
 export default async function InputKasir() {
-    const [daftarPelanggan, daftarItem, session] = await Promise.all([
-        MenuService.getDaftarPelanggan(),
-        MenuService.getDaftarItems(),
-        getSession(),
+    console.time("pelanggan");
+    console.time("item");
+    console.time("session");
+
+     const [daftarPelanggan, daftarItem, session] = await Promise.all([
+        MenuService.getDaftarPelanggan().finally(() => console.timeEnd("pelanggan")),
+        MenuService.getDaftarItems().finally(() => console.timeEnd("item")),
+        getSession().finally(() => console.timeEnd("session")),
     ]);
     const user = session ? { nama: session.nama } : null;
 
